130796899ad88a69be6c9b62a3547a7216f52c68
Giovani de Oliveira <giovanioliveira@outlook.com.br>
2023-04-29 21:21:02 -0300

    Downgrade targetSdk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,9 +13,9 @@ android {
     defaultConfig {
         applicationId = "dev.gico.wcorreios"
         minSdk = 30
-        targetSdk = 33
-        versionCode = 2
-        versionName = "2.0"
+        targetSdk = 30
+        versionCode = 3
+        versionName = "3.0"
         vectorDrawables {
             useSupportLibrary = true
         }
